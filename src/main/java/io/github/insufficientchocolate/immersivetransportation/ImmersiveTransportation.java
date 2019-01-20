0be1742b2aa6c8fde47c9d3fca84795536b7f125
insufficientchocolate <tony84727@gmail.com>
2019-01-20 22:35:09 +0800

    add main mod file

---
+++ src/main/java/io/github/insufficientchocolate/immersivetransportation/ImmersiveTransportation.java
@@ -0,0 +1,12 @@
+package io.github.insufficientchocolate.immersivetransportation;
+
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid=ImmersiveTransportation.MODID,version = ImmersiveTransportation.VERSION,name = ImmersiveTransportation.NAME)
+public class ImmersiveTransportation
+{
+	public static final String MODID = "immersivetransportation";
+	public static final String VERSION = "0.0.0";
+	public static final String NAME = "Immersive Transportation";
+}
